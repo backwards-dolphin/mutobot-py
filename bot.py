3e18c7a81faaf872c8f109321ce68560ea8416f9
m-aou <colinrocks101@hotmail.com>
2018-07-12 22:09:04 -0400

    add game message

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
+    await client.change_presence(game=discord.Game(name='Feeding on Tama | .help'))
 
 @client.event
 async def on_member_join(member):
