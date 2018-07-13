b703e585070109db1b4c40da1a5753273d8b6820
m-aou <colinrocks101@hotmail.com>
2018-07-12 22:10:08 -0400

    update game msg again

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    await client.change_presence(game=discord.Game(name='Feeding on Tama | .help'))
+    await client.change_presence(game=discord.Game(name='in Tama | .help'))
 
 @client.event
 async def on_member_join(member):
