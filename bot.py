89b78cc6e87e612c820b91e284358f4211dd5fc0
m-aou <colinrocks101@hotmail.com>
2018-07-14 19:37:00 -0400

    updated poll, added permission

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def on_command_error(error,ctx):
     elif isinstance(error, commands.CommandNotFound):
         pass
     else:
-        await client.send_message(ctx.message.channel,"Unknown error!")
+        print("Some error has been thrown!")
 
 @client.event
 async def on_message(message):
@@ -114,27 +114,33 @@ async def stamp(ctx, *, msg):
         await client.say("Invalid Input!")
     await client.say(string)
 
-@client.command(pass_context=True)  # Polling command taken from github user Vexs
-async def quickpoll(ctx, question, *options: str):
-    if len(options) <= 1:
+@commands.has_permissions(manage_roles = True)
+@client.command(pass_context=True)  # Taken from github/Vexs but formatted
+async def quickpoll(ctx, *, options: str):
+
+    list1 = options.split("|")
+    question = list1[0] # Get the question
+    entries = list1[1:] # Get the answers
+
+    if len(entries) <= 1:
         await client.say('You need more than one option to make a poll!')
         return
-    if len(options) > 10:
+    if len(entries) > 11:
         await client.say('You cannot make a poll for more than 10 things!')
         return
 
-    if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+    if len(entries) == 2 and entries[0] == 'yes' and entries[1] == 'no':
         reactions = ['✅', '❌']
     else:
         reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
 
-    description = []
-    for x, option in enumerate(options):
-        description += '\n {} {}'.format(reactions[x], option)
-    embed = discord.Embed(title=question, description=''.join(description), color=0xffdd88)
+    description = ""
+    for x, option in enumerate(entries):
+        description += '\n{} {}'.format(reactions[x], option)
+    embed = discord.Embed(title=question, description=description, color=0xffdd88)
     embed.set_thumbnail(url="https://i.imgur.com/LpZoKi7.png")
     react_message = await client.say(embed=embed)
-    for reaction in reactions[:len(options)]:
+    for reaction in reactions[:len(entries)]:
         await client.add_reaction(react_message, reaction)
     embed.set_footer(text='Poll ID: {}'.format(react_message.id))
     await client.edit_message(react_message, embed=embed)
